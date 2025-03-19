6da704cbe1ee248b2e4da8610754657b7112494a
Uwe Rosner <oscottmusic@posteo.de>
2025-03-19 15:33:06 +0100

    Comments

--- src/amigautils/AmigaFile.h
+++ src/amigautils/AmigaFile.h
@@ -92,6 +92,13 @@ private:
 
   void cleanup();
 
+  /**
+   * Safely acquire a lock (`SHARED_LOCK`) even if the final path length
+   * is > 255 chars. If path object can't be locked, it returns ZERO.
+   * 
+   * This method originates from `OpenFromLongName()`, described in 
+   * "Rom Kernel Reference Manual: DOS" from 2024 by Thomas Richter.
+   */
   BPTR lockFromLongName(const char* pPath);
 };
 
